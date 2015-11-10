cbf17962aaa40f1b78a614f0e42da98e089e8d5e
Federico Frenguelli <synasius@gmail.com>
2015-11-10 17:22:35 +0100

    first commit with initial migration that adds an additional index

---
+++ person/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Person',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+            ],
+        ),
+        migrations.RunSQL('CREATE INDEX my_custom_index ON person_person (name);'),
+    ]
